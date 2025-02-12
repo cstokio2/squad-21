222ef437b0060bc00c543a096478eda7985c6ea1
cstokio2 <ti.cassiosousa@gmail.com>
2025-02-12 20:08:29 -0300

    Atualização de modelagem

--- modelagemBD/ModelagemFisica.sql
+++ modelagemBD/ModelagemFisica.sql
@@ -1,71 +1,69 @@
 CREATE database Conectando_Talentos;
 use Conectando_Talentos;
 
--- Tabela Login
-CREATE TABLE Login (
-    ID_Login int PRIMARY KEY auto_increment,
-    Email VARCHAR(100),
-    Senha VARCHAR(100)
-);
-
--- Tabela Candidato
 CREATE TABLE Candidato (
-    ID_Refugiado int PRIMARY KEY auto_increment,
-    Nome VARCHAR(255),
-    Nacionalidade VARCHAR(100),
-    Telefone VARCHAR(50),
-    Whatsapp VARCHAR(50),
-    CEP VARCHAR(20),
-    Estado VARCHAR(50),
-    Cidade VARCHAR(100),
-    Numero VARCHAR(10),
-    Bairro VARCHAR(100),
-    RUA VARCHAR(100),
-    Documento VARCHAR(50),
-    Numero_Documento VARCHAR(50),
-    Email VARCHAR(100),
-    Senha VARCHAR(100),
-    fk_Login_ID_Login int,
-    FOREIGN KEY (fk_Login_ID_Login) REFERENCES Login(ID_Login)
+   ID_Candidato INT PRIMARY KEY,
+   Nome VARCHAR(100),
+   Nacionalidade VARCHAR(50),
+   Telefone VARCHAR(20),
+   Whatsapp VARCHAR(20),
+   Endereco VARCHAR(200),
+   Documento VARCHAR(20),
+   Numero_Documento VARCHAR(20),
+   Email VARCHAR(100),
+   Senha VARCHAR(100),
+   CEP VARCHAR(20),
+   Estado VARCHAR(50),
+   Cidade VARCHAR(50),
+   Numero VARCHAR(10),
+   Bairro VARCHAR(50),
+   RUA VARCHAR(100)
 );
 
--- Tabela Empresa
 CREATE TABLE Empresa (
-    ID_Empresa int PRIMARY KEY auto_increment,
-    Nome_Fantasia VARCHAR(255),
-    Razao_Social VARCHAR(255),
-    Nome_Contato VARCHAR(255),
-    Cargo_Contato VARCHAR(100),
-    Telefone VARCHAR(50),
-    Whatsapp VARCHAR(50),
-    CEP VARCHAR(20),
-    Estado VARCHAR(50),
-    Cidade VARCHAR(100),
-    Numero VARCHAR(10),
-    Bairro VARCHAR(100),
-    RUA VARCHAR(100),
-    CNPJ VARCHAR(20),
-    Email VARCHAR(100),
-    Senha VARCHAR(100),
-    fk_Login_ID_Login int,
-    img_empresa blob,
-    FOREIGN KEY (fk_Login_ID_Login) REFERENCES Login(ID_Login)
+   ID_Empresa INT PRIMARY KEY,
+   CNPJ VARCHAR(20),
+   Email VARCHAR(100),
+   Nome_Fantasia VARCHAR(100),
+   img_empresa VARCHAR(200),
+   Senha VARCHAR(100),
+   Razao_Social VARCHAR(100),
+   Nome_Contato VARCHAR(100),
+   Cargo_Contato VARCHAR(100),
+   Telefone VARCHAR(20),
+   Whatsapp VARCHAR(20),
+   Endereco VARCHAR(200),
+   CEP VARCHAR(20),
+   Estado VARCHAR(50),
+   Cidade VARCHAR(50),
+   Numero VARCHAR(10),
+   Bairro VARCHAR(50),
+   RUA VARCHAR(100)
 );
 
--- Tabela Vaga
+CREATE TABLE Login (
+   ID_Login INT PRIMARY KEY,
+   Email VARCHAR(100),
+   Senha VARCHAR(100),
+   fk_ID_Candidato INT,
+   fk_ID_Empresa INT,
+   FOREIGN KEY (fk_ID_Candidato) REFERENCES Candidato(ID_Candidato),
+   FOREIGN KEY (fk_ID_Empresa) REFERENCES Empresa(ID_Empresa)
+);
+
 CREATE TABLE Vaga (
-    ID_Vaga int PRIMARY KEY auto_increment,
-    Remuneracao VARCHAR(100),
-    Requisitos VARCHAR(255),
-    Localizacao VARCHAR(100),
-    Descricao TEXT,
-    Email_Recrutador VARCHAR(100),
-    Cargo VARCHAR(100),
-    Turno VARCHAR(50),
-    Jornada VARCHAR(50),
-    Tipo_Vinculo VARCHAR(50),
-    Beneficios VARCHAR(255),
-    Limite_Inscricao DATE,
-    fk_Empresa_ID_Empresa int,
-    FOREIGN KEY (fk_Empresa_ID_Empresa) REFERENCES Empresa(ID_Empresa)
+   ID_Vaga INT PRIMARY KEY,
+   Tipo_Vinculo VARCHAR(50),
+   Jornada VARCHAR(50),
+   Turno VARCHAR(50),
+   Cargo VARCHAR(100),
+   Beneficios VARCHAR(200),
+   Remuneracao DECIMAL(10, 2),
+   Localizacao VARCHAR(200),
+   Requisitos TEXT,
+   Descricao TEXT,
+   Email_Recrutador VARCHAR(100),
+   Limite_Inscricao DATE,
+   fk_ID_Empresa INT,
+   FOREIGN KEY (fk_ID_Empresa) REFERENCES Empresa(ID_Empresa)
 );
